d36cd8a105dce27036fa405a2ac5b0745987a64f
Luc Patiny <oceane@patiny.com>
2024-03-22 15:31:56 +0100

    fix: finish buzzer task

--- src/tasks/taskBuzzer.cpp
+++ src/tasks/taskBuzzer.cpp
@@ -4,34 +4,54 @@
 
 Pwm buzzer = Pwm();  // constructor
 
-#define NB_REPETITIONS_ALARM 3
-
 void TaskBuzzer(void* pvParameters) {
   buzzer.attach(BUZZER_PIN);
 
-  setParameter(PARAM_BUZZER, 1);
-  int repetitions = 0;
+  setParameter(PARAM_BUZZER, BUZZER_SINGLE_NOTE);
 
+  int repetitionsAlarm = 0;
   while (true) {
     switch (getParameter(PARAM_BUZZER)) {
+      case BUZZER_OFF:
+        // duration of zero turn output off
+        buzzer.note(BUZZER_PIN, NOTE_E, 3, 0, 0);
+        vTaskDelay(10);
+        break;
+      case BUZZER_SINGLE_NOTE:
+        buzzer.note(BUZZER_PIN, NOTE_C, 4, 10, 0);
+        vTaskDelay(100);
+        setParameter(PARAM_BUZZER, BUZZER_OFF);
+        break;
       case BUZZER_ALARM:
         buzzer.note(BUZZER_PIN, NOTE_C, 4, 10, 0);
         vTaskDelay(100);
         buzzer.note(BUZZER_PIN, NOTE_D, 4, 10, 0);
         vTaskDelay(100);
-        repetitions++;
+        repetitionsAlarm++;
 
-        Serial.println(repetitions);
-        if (repetitions == NB_REPETITIONS_ALARM) {
+        if (repetitionsAlarm == NB_REPETITIONS_ALARM) {
           setParameter(PARAM_BUZZER, BUZZER_OFF);
-          repetitions = 0;
+          repetitionsAlarm = 0;
         }
-
         break;
-      case BUZZER_OFF:
+      case BUZZER_SCALE:
+        for (int i = 0; i < nbNotesScale; i++) {
+          buzzer.note(BUZZER_PIN, scale[i], scaleOctaves[i], 10, 0);
+          vTaskDelay(100);
+        }
+        setParameter(PARAM_BUZZER, BUZZER_OFF);
+        break;
+      case BUZZER_BOOT:
+        for (int i = 0; i < nbNotesBoot; i++) {
+          buzzer.note(BUZZER_PIN, bootNotes[i], bootOctaves[i], 10, 0);
+          vTaskDelay(100);
+        }
+        setParameter(PARAM_BUZZER, BUZZER_OFF);
+        break;
       default:
-        buzzer.note(BUZZER_PIN, NOTE_E, 4, 0, 0);
-        vTaskDelay(100);
+        Serial.println("Unknown buzzer mode");
+        setParameter(PARAM_BUZZER, BUZZER_OFF);
+        vTaskDelay(10);
         break;
     }
   }
@@ -46,33 +66,4 @@ void taskBuzzer() {
                           2,  // Priority, with 3 (configMAX_PRIORITIES - 1)
                               // being the highest, and 0 being the lowest.
                           NULL, 1);
-}
-
-void buzzerModes(Pwm buzzer) {
-  switch (getParameter(PARAM_BUZZER)) {
-    case BUZZER_OFF:
-      buzzer.note(BUZZER_PIN, NOTE_E, 4, 0, 0);
-      vTaskDelay(100);
-      break;
-    case BUZZER_SINGLE_NOTE:
-      Serial.println("Buzzer single note");
-      buzzer.note(BUZZER_PIN, NOTE_C, 4, 10, 0);
-      break;
-    case BUZZER_ALARM:
-      for (int i = 0; i < 3; i++) {
-        buzzer.note(BUZZER_PIN, NOTE_C, 4, 500, 500);
-      }
-      break;
-    case BUZZER_SCALE:
-      for (int i = 0; i < notes; i++) {
-        note_t note = cScale[i];
-        int octave = cScaleOctaves[i];
-        buzzer.note(BUZZER_PIN, note, octave, 10,
-                    0);  // auto attaches to ch 0
-      }
-      break;
-    default:
-      Serial.println("Unknown buzzer mode");
-      break;
-  }
 }
--- src/tasks/taskBuzzer.h
+++ src/tasks/taskBuzzer.h
@@ -4,16 +4,24 @@
 
 #define BUZZER_PIN D0
 
+#define NB_REPETITIONS_ALARM 4
+
 enum BuzzerModes {
   BUZZER_OFF = 0,
   BUZZER_SINGLE_NOTE = 1,
   BUZZER_ALARM = 2,
   BUZZER_SCALE = 3,
+  BUZZER_BOOT = 4
 };
 
-const int notes = 8;
-const note_t cScale[notes] = {NOTE_C, NOTE_D, NOTE_E, NOTE_F,
-                              NOTE_G, NOTE_A, NOTE_B, NOTE_C};
-const int cScaleOctaves[notes] = {4, 4, 4, 4, 4, 4, 4, 5};
+const int nbNotesScale = 8;
+const note_t scale[nbNotesScale] = {NOTE_C, NOTE_D, NOTE_E, NOTE_F,
+                                    NOTE_G, NOTE_A, NOTE_B, NOTE_C};
+const int scaleOctaves[nbNotesScale] = {4, 4, 4, 4, 4, 4, 4, 5};
+
+const int nbNotesBoot = 7;
+const note_t bootNotes[nbNotesBoot] = {NOTE_C, NOTE_E, NOTE_G, NOTE_C,
+                                       NOTE_G, NOTE_E, NOTE_C};
+const int bootOctaves[nbNotesBoot] = {4, 4, 4, 5, 4, 4, 4};
 
 void TaskBuzzer();
